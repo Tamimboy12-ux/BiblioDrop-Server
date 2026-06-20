b4ada41d9997b25462bd6aa0164b4f18163edc2f
unknown <rtamim504@gmail.com>
2026-06-20 17:14:10 +0600

    Successfully connected mongodb in backend

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ dns.setServers(['8.8.8.8', '8.8.4.4'])
 
 const express = require('express');
 const app = express()
-
+const dotenv = require("dotenv")
+dotenv.config()
 
 const port = 5000
 
@@ -12,7 +13,30 @@ const port = 5000
 
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGO_DB_URI
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
